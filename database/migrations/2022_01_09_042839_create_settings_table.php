a5203e43b3f5d629a2a6c555a7453c8a1d2382bd
Allyson Maciel Guimarães <bsinet@hotmail.com>
2022-01-09 01:31:17 -0300

    Add setting model

---
+++ database/migrations/2022_01_09_042839_create_settings_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSettingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('settings', function (Blueprint $table) {
+            $table->id();
+            $table->string("balance_strategy")->default('priority');
+            $table->integer('revalidate_seconds')->default(59);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('settings');
+    }
+}
