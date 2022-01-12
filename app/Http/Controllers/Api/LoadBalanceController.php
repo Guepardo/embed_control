2615f92896554cf3f96fa9a77c246216148d735f
Allyson Maciel Guimarães <bsinet@hotmail.com>
2022-01-12 02:08:12 -0300

    sync

--- app/Http/Controllers/Api/LoadBalanceController.php
+++ app/Http/Controllers/Api/LoadBalanceController.php
@@ -2,29 +2,31 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Http\Controllers\Controller;
 use App\Models\Setting;
 use App\Models\StreamPoint;
-use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Cache;
 
 class LoadBalanceController extends Controller
 {
     public function index()
     {
+        $payload = Cache::remember('payload', 5, function () {
+            $streamPoint = StreamPoint::active()
+                ->byActive()
+                ->byPriority()
+                ->first();
 
-        $streamPoint = StreamPoint::active()
-            ->byActive()
-            ->byPriority()
-            ->first();
+            $setting = Setting::first();
 
-        $setting = Setting::first();
-
-        $payload = [
-            'setting' => $setting->toArray(),
-            'stream_point' => [
-                'cdn_host' => $streamPoint->cdn_host,
-                'name' => $streamPoint->name,
-            ],
-        ];
+            return [
+                'setting' => $setting->toArray(),
+                'stream_point' => [
+                    'cdn_host' => $streamPoint->cdn_host,
+                    'name' => $streamPoint->name,
+                ],
+            ];
+        });
 
         return $payload;
     }
